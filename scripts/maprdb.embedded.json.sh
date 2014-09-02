dc1bd07c15778933dc5c60c110b4b6b13b395563
Andy Pernsteiner <andypern@gmail.com>
2014-09-02 15:28:56 -0400

    hack for embedded maprDB

--- scripts/maprdb.embedded.json.sh
+++ scripts/maprdb.embedded.json.sh
@@ -15,8 +15,7 @@ TABLEPATH=${NFSMOUNT}/tables/${TABLENAME}
  fi
 
 maprcli table create -path ${TABLEPATH} -defaultreadperm 'g:mapr | g:root' -defaultwriteperm 'g:mapr | g:root' -defaultappendperm 'g:mapr | g:root' 
-maprcli table cf create -path ${TABLEPATH} -cfname details 
-maprcli table cf create -path ${TABLEPATH} -cfname pricing 
+maprcli table cf create -path ${TABLEPATH} -cfname blob 
 
 /usr/bin/hadoop fs -chmod 777 ${TABLEPATH}
 
@@ -24,20 +23,10 @@ HBVERSION=$( cat /opt/mapr/hbase/hbaseversion )
 
 
 
-
-
-
-export HBASE_HOME=/opt/mapr/hbase/hbase-${HBVERSION}
-CLUSTERNAME=$( cat /opt/mapr/conf/mapr-clusters.conf |awk {'print $1'} )
-
-
-
-
 #import
 
-hadoop jar $HBASE_HOME/hbase-0.94.17-mapr-1405.jar \
-        importtsv -Dimporttsv.separator=, \
-        -Dimporttsv.columns=HBASE_ROW_KEY,details:name,details:category,pricing:price \
-        ${TABLEPATH} \
-        /mapr/${CLUSTERNAME}/drill-beta-demo/data/output/products/products.csv
+
+
+cat /mapr/${CLUSTERNAME}/drill-beta-demo/scripts/embedded.input.txt | /usr/bin/hbase shell
+
 
